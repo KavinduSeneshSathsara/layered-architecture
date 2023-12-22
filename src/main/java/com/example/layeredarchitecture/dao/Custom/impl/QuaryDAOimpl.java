2498758eec1f24b27e68bfd3bcc7c1e1bdb2df07
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2023-12-22 22:15:51 +0530

    complete

--- src/main/java/com/example/layeredarchitecture/dao/Custom/impl/QuaryDAOimpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/Custom/impl/QuaryDAOimpl.java
@@ -15,13 +15,13 @@ public class QuaryDAOimpl implements QurayDAO {
         ResultSet resultSet = SQLUtil.execute("SELECT customer.*, orders.* FROM customer RIGHT JOIN orders ON customer.id = orders.customerID;");
         List<CustomerOrderDetailsDTO> dtoList = new ArrayList<>();
         while (resultSet.next()){
-            dtoList.add(new CustomerOrderDetailsDTO(
+            dtoList.add( new CustomerOrderDetailsDTO(
                     resultSet.getString(1),
                     resultSet.getString(2),
                     resultSet.getString(3),
                     resultSet.getString(4),
                     resultSet.getString(5)
-            ));
+            ) );
         }
         return dtoList;
     }
