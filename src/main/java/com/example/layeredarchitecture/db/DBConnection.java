17dd9c526251b8cae9aebbebed29aea3170f9743
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2023-12-22 15:03:21 +0530

    gd

--- src/main/java/com/example/layeredarchitecture/db/DBConnection.java
+++ src/main/java/com/example/layeredarchitecture/db/DBConnection.java
@@ -3,8 +3,7 @@ package com.example.layeredarchitecture.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-aa
+gd
 public class DBConnection {
     private static DBConnection dbConnection;
     private final Connection connection;
