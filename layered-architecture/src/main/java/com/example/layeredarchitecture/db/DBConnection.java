81796fd48e8f663d507228fafa8baf4e86dbafb5
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2023-12-22 14:52:28 +0530

    Update DBConnection.java

--- layered-architecture/src/main/java/com/example/layeredarchitecture/db/DBConnection.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/db/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "Ashen", "Ashen@2004");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "Kavindu44444");
     }
 
     public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
