a10954575837569f4a1f313323be40d093537ded
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2023-12-25 12:31:07 +0530

    fully refracted

--- src/main/java/com/example/layeredarchitecture/dao/Custom/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.layeredarchitecture.dao.Custom;
-
-import com.example.layeredarchitecture.dto.OrderDetailDTO;
-
-import java.sql.SQLException;
-
-public interface OrderDetailDAO {
-    boolean saveOrderDetail(String orderId, OrderDetailDTO detailDTO) throws SQLException, ClassNotFoundException;
-}
