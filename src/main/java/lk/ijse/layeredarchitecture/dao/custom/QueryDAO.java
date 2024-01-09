31a9a7cb5a0b7440df9724dbe5570ce9e7c39f9b
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2024-01-09 14:26:49 +0530

    fully refracted

---
+++ src/main/java/lk/ijse/layeredarchitecture/dao/custom/QueryDAO.java
@@ -0,0 +1,11 @@
+package lk.ijse.layeredarchitecture.dao.custom;
+
+import lk.ijse.layeredarchitecture.dao.SuperDAO;
+import lk.ijse.layeredarchitecture.dto.CustomDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface QueryDAO extends SuperDAO {
+//    public ArrayList<Search> getOrderDetail() throws SQLException, ClassNotFoundException;
+}
---
+++ src/main/java/lk/ijse/layeredarchitecture/dao/custom/impl/QueryDAOImpl.java
@@ -0,0 +1,18 @@
+package lk.ijse.layeredarchitecture.dao.custom.impl;
+
+import lk.ijse.layeredarchitecture.dao.custom.QueryDAO;
+import lk.ijse.layeredarchitecture.dto.CustomDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class QueryDAOImpl implements QueryDAO {
+//    @Override
+//    public void customerOrderDetails(CustomDTO customDTO) throws SQLException, ClassNotFoundException {
+//        //join query
+//            System.out.println();
+//    }
+
+}
+
+
